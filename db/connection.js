82ad3c25c53fca37e675c7e7f9e315f5a1f2e377
NiallColman49 <103330229+NiallColman49@users.noreply.github.com>
2022-08-01 16:58:46 +0100

    Update connection.js

--- db/connection.js
+++ db/connection.js
@@ -1,8 +1,6 @@
 const { Pool } = require("pg");
 const ENV = process.env.NODE_ENV || "development";
 
-console.log(ENV);
-
 require("dotenv").config({
   path: `${__dirname}/../.env.${ENV}`,
 });
@@ -13,15 +11,3 @@ if (!process.env.PGDATABASE) {
 
 module.exports = new Pool();
 
-// {
-//   description: 'The man, the Mitch, the legend',
-//   slug: 'mitch'
-// },
-// {
-//   description: 'Not dogs',
-//   slug: 'cats'
-// },
-// {
-//   description: 'what books are made of',
-//   slug: 'paper'
-// }
