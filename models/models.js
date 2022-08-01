45df134dee1dc1b00c83bb6af0c2924b6c5c80c0
NiallColman49 <103330229+NiallColman49@users.noreply.github.com>
2022-08-01 16:59:31 +0100

    Update models.js

--- models/models.js
+++ models/models.js
@@ -3,7 +3,6 @@
 const db = require("../db/connection.js");
 
 exports.pullAllTopics = () => {
-  console.log("I am in the model");
   return db.query("SELECT * FROM topics;").then((topics) => {
     return topics.rows;
   });
